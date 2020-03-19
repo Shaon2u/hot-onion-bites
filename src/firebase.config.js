26562640951c68c7272575ba318fe80505b8e1bd
Shaon H <shaon2u@gmail.com>
2020-03-20 01:03:26 +0800

    Initialize project using Create React App

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDq3cQZxd69JY3nRZiPauoHoBK72MDQn3E",
+    authDomain: "ema-john-ecom.firebaseapp.com",
+    databaseURL: "https://ema-john-ecom.firebaseio.com",
+    projectId: "ema-john-ecom",
+    storageBucket: "ema-john-ecom.appspot.com",
+    messagingSenderId: "562933291583",
+    appId: "1:562933291583:web:bf7bda1ebfa9c129fff68a"
+  };
+
+  export default firebaseConfig;
